164a60081273d98fa8ea0d3a7049f0c6731986e8
Anna <apelsann@ya.ru>
2017-01-21 17:07:57 +0300

    2101

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,27 @@
-$(".calculation__open").on("click", function(){
-	$(this).toggleClass("calculation__open--open");
-	$(this).parents(".calculation__item").find(".calculation__content").slideToggle("fast");
-});
+//custom select
+
+function customSelectInit () {
+    $(".js_chosen_select").chosen({
+        disable_search_threshold: 100,
+        no_results_text: "Ничего не найдено",
+        placeholder_text_single: " "
+    });
+
+    $('.js_chosen_select').on('chosen:showing_dropdown', function(e, params) {
+        var el = params.chosen.container[0];
+        $(el).find('li').wrapInner('<span></span>');
+        if (!$(el).find('.js_custom_scroll').length) {
+            $(el).find('.chosen-results').wrap('<div class="js_custom_scroll"></div>');
+            customScrollInit();
+        }
+    });
+    var list = $('.js_chosen_select').next();
+    var input = list.find('input');
+    list.find('input').on('keyup',function(){
+        list.find('li').wrapInner('<span></span>');
+        input.focus();
+     })
+
+
+}
+customSelectInit();
